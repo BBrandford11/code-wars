180a2f5138ac8029ccb22b1567fc0fc43ddc8a36
BBrandford11 <brandfordbryce@gmail.com>
2022-05-16 20:49:18 -0600

    adds comments and better way to solve

--- 7kyu/reverse_words.js
+++ 7kyu/reverse_words.js
@@ -4,11 +4,17 @@
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
 function reverseWords(str) {
+  //split string into array
   let split = str.split(" ");
+  //loop thru array and split each word and reverse them
   for (let x = 0; x < split.length; x++) {
     split[x] = split[x].split("").reverse().join("");
   }
+  // join back together
   return split.join(" ");
 }
 
-
+//better way using map and reversing inside of the return
+// function reverseWords(str) {
+//   return str.split(' ').map( str => str.split('').reverse().join('') ).join(' ');
+// }
