f9a043dc9dc456cd22fbbdfa4f28b6141395bf22
BBrandford11 <brandfordbryce@gmail.com>
2022-05-22 11:20:05 -0600

    solved infinite with <=

--- Algorithms/binary_search.js
+++ Algorithms/binary_search.js
@@ -6,18 +6,17 @@ const binary = (array, num) => {
   let left = 0;
   let right = array.length - 1;
 
-
   while (left <= right) {
     let middle = Math.floor((left + right) / 2);
-    console.log(left, right)
+    console.log(left, right);
     if (array[middle] === num) {
       return true;
     }
 
-    if (num < array[middle]) {
-      left = middle;
-    }
     if (num > array[middle]) {
+      left = middle + 1;
+    }
+    if (num < array[middle]) {
       right = middle - 1;
     }
   }
@@ -26,7 +25,7 @@ const binary = (array, num) => {
 
 const nums = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26
+  23, 24, 25, 26,
 ];
 
 console.log(binary(nums, 10));
