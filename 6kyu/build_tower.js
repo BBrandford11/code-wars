413c6bd68e2a14cd0275fc623bff7ba8946a4fe1
BBrandford11 <brandfordbryce@gmail.com>
2022-05-23 17:56:25 -0600

    hard question tower builder

---
+++ 6kyu/build_tower.js
@@ -0,0 +1,32 @@
+// Build Tower
+
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+// confusing question... learn repeate and think abiut the individual think of the space and the stars to be 2 different things and figure out how to loop thru them
+function towerBuilder(nFloors) {
+  let array = [];
+  for (let i = nFloors; i >= 1; i--) {
+    let space = (nFloors - i);
+    let stars = i * 2 - 1;
+    array.push(" ".repeat(space) + "*".repeat(stars) + " ".repeat(space));
+  }
+  return array.reverse();
+}
