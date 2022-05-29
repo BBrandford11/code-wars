4c158c6f6558a3d397cdf55c82dbffbaea6dcdf3
BBrandford11 <brandfordbryce@gmail.com>
2022-05-29 12:15:56 -0600

    using if with ||

--- 7kyu/vowel_count.js
+++ 7kyu/vowel_count.js
@@ -3,3 +3,22 @@
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 // The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let vowelsCount = 0;
+
+  // enter your magic here
+  for (let x = 0; x < str.length; x++) {
+    if (
+      str[x] === a ||
+      str[x] === e ||
+      str[x] === i ||
+      str[x] === o ||
+      str[x] === u
+    ) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
