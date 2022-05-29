30d4b90943bedfc0494ec800842d74911d015670
BBrandford11 <brandfordbryce@gmail.com>
2022-05-29 12:17:58 -0600

    forgot ''

--- 7kyu/vowel_count.js
+++ 7kyu/vowel_count.js
@@ -10,11 +10,11 @@ function getCount(str) {
   // enter your magic here
   for (let x = 0; x < str.length; x++) {
     if (
-      str[x] === a ||
-      str[x] === e ||
-      str[x] === i ||
-      str[x] === o ||
-      str[x] === u
+      str[x] === "a" ||
+      str[x] === "e" ||
+      str[x] === "i" ||
+      str[x] === "o" ||
+      str[x] === "u"
     ) {
       vowelsCount++;
     }
