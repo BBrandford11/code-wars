49c15d94405b80c9d8f96b7ac2d596825366de5a
BBrandford11 <brandfordbryce@gmail.com>
2022-05-31 15:16:27 -0600

    added description

--- 8kyu/abbreviate_Two_names.js
+++ 8kyu/abbreviate_Two_names.js
@@ -1,3 +1,14 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
 const abbrevName = (name) => {
   let split = name.split(" ")
   return `${split[0][0].toUpperCase()}. ${split[1][0].toUpperCase()}`
