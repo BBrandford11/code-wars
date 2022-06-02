ce07c63db6d4a146d691db856f53ad8f9d82e01f
BBrandford11 <brandfordbryce@gmail.com>
2022-06-01 18:28:10 -0600

    added good way using sort

--- 8kyu/small_int.js
+++ 8kyu/small_int.js
@@ -18,3 +18,11 @@ class SmallestIntegerFinder {
     return result;
   }
 }
+
+// class SmallestIntegerFinder {
+//   findSmallestInt(args) {
+//     args.sort(function(a, b) {
+//     return a - b; } )
+//     return args[0];
+//   }
+// }
