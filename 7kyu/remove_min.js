f5de0ae543d8d06951c0fd9197ee53e6ee84fe7e
BBrandford11 <brandfordbryce@gmail.com>
2022-06-07 10:39:02 -0600

    warm up

--- 7kyu/remove_min.js
+++ 7kyu/remove_min.js
@@ -23,6 +23,25 @@ function removeSmallest(numbers) {
       nums = x;
     }
   }
+  numbers.slice(1, nums)
 
   return result;
 }
+
+
+// function removeSmallest(numbers) {
+//   var smallest;
+//   var newNums = [];
+//   numbers.forEach(function(num) {
+//     if (smallest > num || smallest === undefined) {
+//       smallest = num;
+//     }
+//   });
+//   var index = numbers.indexOf(smallest);
+//   numbers.forEach(function(n, i) {
+//     if (i !== index) {
+//       newNums.push(n);
+//     }
+//   });
+//   return newNums;
+// }
