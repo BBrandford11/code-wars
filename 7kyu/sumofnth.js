eb80bf7b7670d86cc896a1dba17f260c85df9ae0
BBrandford11 <brandfordbryce@gmail.com>
2022-06-30 16:14:29 -0600

    blah

--- 7kyu/sumofnth.js
+++ 7kyu/sumofnth.js
@@ -1,5 +1,3 @@
-Task:
-
 // Your task is to write a function which returns the sum of following series upto nth term(parameter).
 
 // Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
@@ -18,9 +16,25 @@ Task:
 // 2 --> 1 + 1/4 --> "1.25"
 // 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
 
+// function SeriesSum(n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   let result = 1;
+//   let numbs = [1 / 4, 1 / 7, 1 / 10, 1 / 13, 1 / 16];
+//   if (n > 1) {
+//     for (let x = 0; x < n; x++) {
+//       result += numbs[x];
+//     }
+//   }
+//   return result.toFixed(2)
 
+// }
 
-function SeriesSum(n)
-{
-  // Happy Coding ^_^
+function SeriesSum(n) {
+  var sum = 0;
+  for(var i = 0; i < n; i++) {
+    sum += 1 / (3 * i + 1);
+  }
+  return sum.toFixed(2);
 }
