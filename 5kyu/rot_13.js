4578df3a319884189f31013a58692e24719f0614
BBrandford11 <brandfordbryce@gmail.com>
2022-07-05 21:20:36 -0600

    added solution tho I think its cheating

--- 5kyu/rot_13.js
+++ 5kyu/rot_13.js
@@ -2,29 +2,45 @@
 
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
-function rot13(message) {
-  //your code here
-  let result = [];
-  for (let x = 0; x < message.length; x++) {
-    let number = message.charCodeAt(x);
-    console.log(number);
-    if (message[x] === " ") {
-      result.push(" ")
-    }
-    if (number >= 78 && number < 91) {
-      result.push(String.fromCharCode(0 + (number - 106) + 93));
-      console.log("if");
-    } else if (number >= 65 && number < 78) {
-      result.push(String.fromCharCode(number + 13));
-      console.log("else");
-    } else if (number > 106) {
-      result.push(String.fromCharCode(0 + (number - 106) + 93));
-    } else {
-      result.push(String.fromCharCode(number + 13));
-    }
-  }
+// function rot13(message) {
+//   //your code here
+//   let result = [];
+//   for (let x = 0; x < message.length; x++) {
+//     let number = message.charCodeAt(x);
+//     console.log(number);
+//     if (message[x] === " ") {
+//       result.push(" ")
+//     }
+//     if (number >= 78 && number < 91) {
+//       result.push(String.fromCharCode(0 + (number - 106) + 93));
+//       console.log("if");
+//     } else if (number >= 65 && number < 78) {
+//       result.push(String.fromCharCode(number + 13));
+//       console.log("else");
+//     } else if (number > 106) {
+//       result.push(String.fromCharCode(0 + (number - 106) + 93));
+//     } else {
+//       result.push(String.fromCharCode(number + 13));
+//     }
+//   }
 
-  console.log(message);
-  console.log(result, "result");
-  return result.join("");
+//   console.log(message);
+//   console.log(result, "result");
+//   return result.join("");
+// }
+
+
+var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXWZ";
+var cipher   = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXWZABCDEFGHIJKLM";
+
+function rot13(message){
+  return message.split('').map(function(char) {
+    var i = alphabet.indexOf(char);
+    if (i < 0) {
+      // not in alphabet, return char
+      return char;
+    }
+    
+    return cipher[i];
+  }).join('');
 }
