2619a2117d82836deb693085874f4ee5fcd59ee4
BBrandford11 <brandfordbryce@gmail.com>
2022-07-26 17:49:12 -0600

    solved

--- 8kyu/oppositesAttract.js
+++ 8kyu/oppositesAttract.js
@@ -3,5 +3,11 @@
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
 function lovefunc(flower1, flower2) {
-  return flower1 % 2 === 0 && flower2 % 2 === 0 ? true : false;
+  if (flower1 % 2 === 0 && flower2 % 2 === 1) {
+    return true;
+  }
+  if (flower1 % 2 === 1 && flower2 % 2 === 0) {
+    return true;
+  }
+  return false;
 }
