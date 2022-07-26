2bee38e755f820f165bd8dd96386ffa0b45ee141
BBrandford11 <brandfordbryce@gmail.com>
2022-07-26 16:00:02 -0600

    remove ! with 2 solutions

--- 8kyu/removeExclamations.js
+++ 8kyu/removeExclamations.js
@@ -1,5 +1,12 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 function removeExclamationMarks(s) {
-  return s.replace("!", "");
+  let string = "";
+  for (let x = 0; x < s.length; x++) {
+    if (s[x] === "!") {
+    } else {
+      string += s[x];
+    }
+  }
+  return string;
 }
