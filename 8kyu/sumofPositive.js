27eae8a8cd892da54fc5dfefa85c3f68903529ca
BBrandford11 <brandfordbryce@gmail.com>
2022-07-29 12:42:02 -0600

    sum of positive

---
+++ 8kyu/sumofPositive.js
@@ -0,0 +1,17 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+function positiveSum(arr) {
+  let result = 0 
+  for (let x = 0; x < arr.length; x++) {
+    if(arr[x] > 0 ){
+      result += arr[x]
+    }
+    
+  }
+  return result
+}
